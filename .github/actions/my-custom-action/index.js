316dc184ce21f1bbe00c11c3fbe4ccf2e28922e9
muskan-shah2025 <muskanshah2250@gmail.com>
2025-06-09 21:18:45 +0530

    updated

--- .github/actions/my-custom-action/index.js
+++ .github/actions/my-custom-action/index.js
@@ -1,17 +1,17 @@
-const core = require('@actions/core');
+// const core = require('@actions/core');
 
-async function run() {
-  try {
-    const username = core.getInput('username');
-    const password = core.getInput('password');
+// async function run() {
+//   try {
+//     const username = core.getInput('username');
+//     const password = core.getInput('password');
 
-    core.info(`✅ Login attempt for user: ${username}`);
-    core.info(`🔐 Authenticating...`);
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    core.info(`🎉 Successfully authenticated user "${username}"`);
-  } catch (error) {
-    core.setFailed(`❌ Action failed: ${error.message}`);
-  }
-}
+//     core.info(`✅ Login attempt for user: ${username}`);
+//     core.info(`🔐 Authenticating...`);
+//     await new Promise((resolve) => setTimeout(resolve, 1000));
+//     core.info(`🎉 Successfully authenticated user "${username}"`);
+//   } catch (error) {
+//     core.setFailed(`❌ Action failed: ${error.message}`);
+//   }
+// }
 
-run();
+// run();
